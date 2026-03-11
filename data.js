148475258b541cb897d9df378f2132e6bc30c2b3
LIWENDA888 <744223766@qq.com>
2026-03-11 14:10:20 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,14 @@
+// data.js
+const fontLibrary =[
+    { id: "1001", title: "质感拉满的 20 款中文黑体字 (设计师必备)", links:[
+        { type: "百度网盘", url: "https://pan.baidu.com/s/bd_demo" },
+        { type: "夸克网盘", url: "https://pan.quark.cn/s/qk_demo" }
+    ]},
+    { id: "1002", title: "Google Fonts 全球高分宋体合集", links:[
+        { type: "官方页面", url: "https://fonts.google.com" },
+        { type: "百度网盘", url: "https://pan.baidu.com/s/bd_demo2" }
+    ]},
+    { id: "A005", title: "极简现代英文衬线体 - Minimalist Serif", links:[
+        { type: "夸克网盘", url: "https://pan.quark.cn/s/qk_demo3" }
+    ]}
+];
